47279edd642f86b34f8c37d5d9a355bf9bb54613
Anderson <38814895+Storepac@users.noreply.github.com>
2022-10-18 22:16:23 -0300

    itens

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 /* mini menu */
-function openCity(evt, cityName) {
+function openItem(evt, itemName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -28,7 +28,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(itemName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 

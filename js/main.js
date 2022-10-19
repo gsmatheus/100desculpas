0526f4bc41a35a54c4ea4aedaa7ede8809244825
Anderson <38814895+Storepac@users.noreply.github.com>
2022-10-18 22:14:54 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -17,6 +17,23 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 
+/* mini menu */
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+ /* scroll */
 window.addEventListener('scroll', function(){
   var menu = document.querySelector('.header-menu');
   menu.classList.toggle('sticky', window.scrollY > 0);
